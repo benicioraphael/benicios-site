6e00b6f4d71755bffb49d185386ba5bbf33da05f
benicioraphael <raphael2005rj@hotmail.com>
2026-04-23 22:18:40 -0300

    produtos.js

---
+++ produtos.js
@@ -0,0 +1,20 @@
+const produtos = [
+  {
+    nome: "Cookie Tradicional",
+    categoria: "Cookies",
+    preco: 8,
+    lojas: ["centro", "ilha"]
+  },
+  {
+    nome: "Cookie Nutella",
+    categoria: "Cookies",
+    preco: 10,
+    lojas: ["centro", "ilha"]
+  },
+  {
+    nome: "Donut Chocolate",
+    categoria: "Donuts",
+    preco: 12,
+    lojas: ["ilha"]
+  }
+];
